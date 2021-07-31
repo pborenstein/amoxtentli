ea0b50246eca2925736eaba3be47dedaee427923
Philip Borenstein <pborenstein@gmail.com>
2021-07-31 00:35:39 -0400

    wtf happend to my file?

--- post-to-github.js
+++ post-to-github.js
@@ -1,3 +1,5 @@
+// post-to-github.js
+
 // https://forums.getdrafts.com/t/script-step-post-to-github-without-working-copy/3594
 
 function pad(n) {
@@ -19,49 +21,79 @@ const draftStamp = `${dateStr(drafttime)}T${timeStr(drafttime)}`
 
 // const fname = `${dateStr(posttime)}-${timeStr(posttime)}.md`
 const fname = `${draft.uuid.toLowerCase()}.md`
+let url = `https://api.github.com/repos/${githubUser}/${repo}/contents/${path}/${fname}`
+// let url = `https://api.github.com/repos/${githubUser}/${repo}/contents/${path}/foo.md`
 
 const doc = `doc`
 
+//  we're using a single request object
+//  and filling in the changes
 
-const options = {
-    url: `https://api.github.com/repos/${githubUser}/${repo}/contents/${path}/${fname}`,
-    method: 'GET',
-    headers: {
-        'Authorization': `token ${githubKey}`,
-        'Accept': 'application/vnd.github.v3+json'
-    }
+const gRequest = {
+  url: path,
+  method: 'GET',
+  headers: {
+    'Authorization': `token ${githubKey}`,
+    'Accept': 'application/vnd.github.v3+json'
+  }
 }
 
-function getSHA(path) {
-  const request = {
-    url: path,
-    method: 'GET',
-    headers: {
-      'Authorization': `token ${githubKey}`,
-      'Accept': 'application/vnd.github.v3+json'
-    }
-  }
 
+function getSHA(path) {
   let http = HTTP.create()
-  let response = http.request(request)
+      gRequest.url = path
+  let response = http.request(gRequest)
 
   /**/draft.append(`response.statusCode: ${response.statusCode}`)
 
   if (response.statusCode == 404)
-    return "false"  // doesn't exist
+    return false  // doesn't exist
 
   if (response.statusCode == 200) {
     let parsedResponseText = JSON.parse(response.responseText)
-
     return parsedResponseText.sha
   }
 
+  context.cancel()
+  app.displayErrorMessage(`Something went wrong (${response.statusCode})`)
+
 }
 
-// let url = `https://api.github.com/repos/${githubUser}/${repo}/contents/${path}/${fname}`
-let url = `https://api.github.com/repos/${githubUser}/${repo}/contents/${path}/foo.md`
 let sha = getSHA(url)
 
 draft.append(`url: ${url}`)
 draft.append(`sha: ${sha}`)
 
+
+
+// set up the request
+
+gRequest.method = 'PUT'
+gRequest.data = {
+      message: `note created at ${postStamp}`,
+      content: Base64.encode(doc),
+      branch: 'main'
+}
+
+//  add a sha if we need it
+if (sha) {
+  gRequest.sha = sha
+}
+
+
+draft.append(`gRequest: ${JSON.stringify(gRequest, null, 2)}`)
+
+
+if (!sha) {
+  draft.append(`will post as new`)
+} else {
+  draft.append(`overwrite existing`)
+}
+
+let http = HTTP.create()
+let response = http.request(gRequest)
+if (response.statusCode <= 250) {
+  app.displaySuccessMessage('Posted')
+} else {
+  app.displayErrorMessage(`Something went wrong (${response.statusCode})`)
+}
